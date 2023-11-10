b6dde0172cda1dab2759706682676d5aa31b420f
matheushnascimento <matheus.mhddn@gmail.com>
2023-11-10 18:32:43 -0300

    chamada pra função dos botões time control

---
+++ focusTimer/actions.js
@@ -0,0 +1,9 @@
+export function toggleRunning() {
+  console.log("play/stop");
+}
+export function plus5Sec() {
+  console.log("+5sec");
+}
+export function minus5Sec() {
+  console.log("-5sec");
+}
--- focusTimer/events.js
+++ focusTimer/events.js
@@ -1,11 +1,20 @@
 import * as control from "./elements.js";
+import * as el from "./elements.js";
+import * as actions from "./actions.js";
 
 export function registerControls() {
   control.timeControls.addEventListener("click", event => {
-    console.log(event.target.dataset.action);
+    const action = event.target.dataset.action;
+
+    if (typeof action === "undefined") {
+      console.log("não é um botão");
+      return;
+    }
+
+    actions[action]();f
   });
 
   control.soundControls.addEventListener("click", event => {
     console.log(event.target.dataset.action);
-  })
+  });
 }
